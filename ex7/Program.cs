a095c0587c3c82adc45340d1cab5d4f91983f62e
HeronCendrethe <heron.cendrethe@gmail.com>
2019-09-18 15:06:14 -0300

    Visual

---
+++ ex7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o tipo de voto");
+            Console.WriteLine("V - Validos");
+            Console.WriteLine("B - Brancos");
+            Console.WriteLine("N - Nulos");
+            String tipoVoto;
+            tipoVoto = Console.ReadLine();
+            int contValido;
+
+            switch (tipoVoto)
+            {
+                case "V":
+                    
+                    contValido = contValido+1; 
+
+                    break;
+                case "B":
+
+                    break;
+                case "N":
+
+                    break;
+
+            }
+        }
+    }
+}
---
+++ ex7/voto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex7
+{
+    class voto
+    {
+
+        private int votosBrancos;
+        private int votosNulos;
+        private int votosValidos;
+
+        public int VotosBrancos
+        {
+            get
+            {
+                return votosBrancos;
+            }
+
+            set
+            {
+                votosBrancos = value;
+            }
+        }
+
+        public int VotosNulos
+        {
+            get
+            {
+                return votosNulos;
+            }
+
+            set
+            {
+                votosNulos = value;
+            }
+        }
+
+        public int VotosValidos
+        {
+            get
+            {
+                return votosValidos;
+            }
+
+            set
+            {
+                votosValidos = value;
+            }
+        }
+    }
+}
